afcaec74816d65a18aa7a9ed57facf6c2b410aaa
bryson <bryson@pop-os.localdomain>
2024-04-15 12:46:00 +0300

    profile updates

--- movers/movers/urls.py
+++ movers/movers/urls.py
@@ -18,11 +18,17 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path,include
 from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('register/',user_views.register, name='register'),
+    path('profile/',auth_views.LoginView.as_view(template_name='users/profile.html'), name='profile'),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
