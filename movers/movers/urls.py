b2e5ea6731967dcade68da12f8b993ad834b40aa
braeson-nyahera <braebulimo@gmail.com>
2024-08-23 13:47:27 +0300

    profile

--- movers/movers/urls.py
+++ movers/movers/urls.py
@@ -24,7 +24,7 @@ from transportation import views as transport_views
 
 urlpatterns = [
     path('register/',user_views.register, name='register'),
-    path('profile/',auth_views.LoginView.as_view(template_name='users/profile.html'), name='profile'),
+    path('profile/',user_views.profile,name='profile'),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/',user_views.logout_view, name='logout'),
     path('logout-done/',user_views.logout_done, name='logout-done'),
