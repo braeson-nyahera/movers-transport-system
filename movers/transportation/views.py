85a60e2065cc71914afe897ba69be13ba13580d9
braeson-nyahera <braebulimo@gmail.com>
2024-10-28 20:55:54 +0300

    carousel

--- movers/transportation/views.py
+++ movers/transportation/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 from django.views.generic import ListView,DetailView,CreateView,UpdateView
 from .models import order
-import folium, geocoder
+#import folium, geocoder
 from movers import settings
 
 # Create your views here.
